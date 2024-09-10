f5b15fa6bcad8bd3c81d0f34c5e80aa7e3298945
Ghottt8899 <baterdene5200@gmail.com>
2024-09-10 18:14:27 +0800

    asdf

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import FruitList from './FruitList';
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'fruitApp.fruits';
 
 function App() {
+    const [fruits, setFruits] = useState([]);
+    const fruitNameRef = useRef();
+
+    useEffect(() => {
+        const storedFruits = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        if (storedFruits) setFruits(storedFruits);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(fruits));
+    }, [fruits]);
+
+    function toggleFruit(id) {
+        const newFruits = [...fruits];
+        const fruit = newFruits.find(fruit => fruit.id === id);
+        fruit.complete = !fruit.complete;
+        setFruits(newFruits);
+    }
+
+    function handleAddFruits() {
+        const name = fruitNameRef.current.value;
+        if (name === '') return;
+        setFruits(prevFruits => [
+            ...prevFruits,
+            { id: uuidv4(), name: name, complete: false }
+        ]);
+        fruitNameRef.current.value = '';
+    }
+
+    function handleClearFruits() {
+        const newFruits = fruits.filter(fruit => !fruit.complete);
+        setFruits(newFruits);
+    }
+
     return ( <
-        div className = "App" >
+        >
         <
-        header className = "App-header" >
+        FruitList fruits = { fruits }
+        toggleFruit = { toggleFruit }
+        /> <
+        input ref = { fruitNameRef }
+        type = "text" / >
         <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        yu bn andaa <
-        /p> <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> <
-        /header> <
-        /div>
+        button onClick = { handleAddFruits } > Add Fruit < /button> <
+        button onClick = { handleClearFruits } > Clear Completed < /button> <
+        div > { fruits.filter(fruit => !fruit.complete).length }
+        fruits remaining < /div> <
+        />
     );
 }
 
---
+++ src/FruitList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Fruit from './Fruit'
+
+export default function FruitList({ fruits, toggleFruit }) {
+    return (
+        fruits.map(fruit => {
+            return ( <
+                Fruit key = { fruit.id }
+                toggleFruit = { toggleFruit }
+                fruit = { fruit }
+                />
+            );
+        })
+    );
+}
